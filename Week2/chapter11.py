0fbc26acfa44d5b59f249160a3573e02c7aaa006
kimdahyeon <anniy8920@naver.com>
2023-02-10 23:20:17 +0900

    chapter11

---
+++ Week2/chapter11.py
@@ -0,0 +1,34 @@
+from utils import Variable
+import numpy as np
+'''
+여러개의 input과 output으로 변경해보기 => 리스트(튜플)에 넣어서 처리
+'''
+class Function:
+    def __call__(self, inputs):
+        xs= [x.data for x in inputs]
+        ys=self.forward(xs)
+        outputs=[Variable(np.asarray(y)) for y in ys]
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs
+        
+    def forward(self, x):
+        raise NotImplementedError()
+    def backwrad(self, gy):
+        raise NotImplementedError()
+    #덧셈을 해주는 Add 클래스 객체
+class Add(Function): #Function의 모든 인스턴스들을 상속받는다.
+    def forward(self, xs):
+        x0, x1= xs
+        y=x0+x1
+        return (y,)
+#코드 실행
+xs=[Variable(np.array(2)), Variable(np.array(3))] #리스트로 준비
+f=Add()
+ys=f(xs) #(output, )
+y=ys[0]
+print(y.data)
+
+        
