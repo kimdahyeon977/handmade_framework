2f838d070d91fdfcebd87e94ccebaae51003688e
kimdahyeon <anniy8920@naver.com>
2023-02-13 20:57:50 +0900

    chapter18

--- Week2/chapter18.py
+++ Week2/chapter18.py
@@ -1,5 +1,6 @@
 import numpy as np
 import weakref
+import contextlib
 class Function:
     def __call__(self, *inputs):
         xs=[x.data for x in inputs]
@@ -7,11 +8,12 @@ class Function:
         if not isinstance(ys, tuple):
             ys=(ys,)
         outputs=[Variable(as_array(y)) for y in ys] #반환 원소가 하나뿐이라면 해당 원소를 직접 반환
-        self.generation = max([x.generation for x in inputs]) #세대수가 높은 것을 선택
-        for output in outputs:
-            output.set_creator(self) #Add가 저장됨.
-        self.inputs=inputs
-        self.outputs= [weakref.ref(output) for output in outputs] #self.outputs가 대상을 약한 참조로 가리키게 변경 => 함수는 출력변수를 약하게 참조한다.
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs]) #세대수가 높은 것을 선택
+            for output in outputs:
+                output.set_creator(self) #계산들의 '연결'을 만든다.
+            self.inputs=inputs
+            self.outputs= [weakref.ref(output) for output in outputs] #self.outputs가 대상을 약한 참조로 가리키게 변경 => 함수는 출력변수를 약하게 참조한다.
         # 리스트의 원소가 한개라면 첫번째 원소를 반환
         return outputs if len(outputs) >1 else outputs[0]
 class Variable:
@@ -51,15 +53,16 @@ class Variable:
                     x.grad = x.grad + gx #그 다음번부터는 전달된 미분값을 더해줘야함.
                 if x.creator is not None:
                     add_func(x.creator)
-        if not retain_grad:
-            for y in f.outputs:
-                y().grad = None #y는 약한 참조
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None #y는 약한 참조
 def cleargrad(self): #미분값 초기화
     self.grad = None
 def as_array(x):
     if np.isscalar(x):
         return np.array(x)
     return x
+
 class Add(Function):
     def forward(self, x0, x1):
         y= x0+x1
@@ -68,12 +71,30 @@ class Add(Function):
         return gy, gy
 def add(x0, x1):
     return Add()(x0,x1)
-#코드 실행
-x0= Variable(np.array(1.0))
-x1=Variable(np.array(1.0))
-t=add(x0,x1)
-y=add(x0,t)
-y.backward()
+class Square(Function):
+    def forward(self, x):
+        y=x**2
+        return y
+    def backward(self, gy):
+        x=self.inputs[0].data
+        gx = 2*x*gy
+        return gx
+def square(x):
+    return Square()(x) 
 
-print(y.grad , t.grad)
-print(x0.grad , x1.grad)
+class Config:
+    enable_backprop = True
+
+def using_config(name, value):
+    old_value = getattr(Config, name) #name을 getattr함수에 넘겨 Config클래스에서 꺼내온다.
+    setattr(Config, name, value) #새로운 값을 설정
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+def no_grad():
+    return using_config('enable_backprop',False)
+#코드 실행
+with no_grad(): #기울기가 없을때는 이렇게 호출
+    x=Variable(np.array(2.0))
+    y=square(x)
